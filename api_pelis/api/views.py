f37ebbdcf02f604c700be056261aac17e85795d9
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-30 17:50:35 +0100

    Add PeliculaViewSet to views.py and update urls.py

--- api_pelis/api/views.py
+++ api_pelis/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .serializers import PeliculaSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+
+class PeliculaViewSet(viewsets.ModelViewSet):
+    queryset = Pelicula.objects.all()
+    serializer_class = PeliculaSerializer
